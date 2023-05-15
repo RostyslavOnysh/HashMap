828c8dda798048126456ee3568a5cfec109ca35d
Ros <rospsix@gmail.com>
2023-05-15 13:30:52 +0100

    #1  Explain what a map is and how it works

---
+++ main/java/Theory/WhatIsMap/Main.java
@@ -0,0 +1,17 @@
+package Theory.WhatIsMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        /* интерфейс map описує інтерфейс при роботі структур данних які оперують таким поняттям
+        як пара  ключ значення.Тобто має бути ключ і по цьому ключу буде знаходитись певне значення.
+        Ключі в мапах будуть унікальними.
+         1. Країна це ключ
+         2. Столиця це значення
+         */
+
+        Map<String, String> capitalSities = new HashMap<>();
+    }
+}
