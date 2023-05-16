7a485d699a7d5c43b07be6566201f21d0a380ce8
Rostyslav <98691406+RostyslavOnysh@users.noreply.github.com>
2023-05-16 20:04:08 +0100

    Update MarketSupplying.java

--- main/java/HomeWork/Store/MarketSupplying.java
+++ main/java/HomeWork/Store/MarketSupplying.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 public class MarketSupplying {
     public Map<String, Integer> supplyFruits(Map<String, Integer> storage, int supply) {
-        //write your code here
       for (String key : storage.keySet()) {
           int actualValue = storage.get(key);
           int updatedValue = actualValue + supply;
@@ -13,28 +12,3 @@ public class MarketSupplying {
         return storage;
     }
 }
-
-
-/*
-Це другий варіант рішення :
-
- for (Map.Entry<String, Integer> entry : storage.entrySet()) {
-      String key = entry.getKey();
-      int actualValue = entry.getValue();
-      int updatedValue = actualValue + supply;
-      storage.put(key, updatedValue);
-    }
-    return storage;
-  }
-}
- */
-
-
-/*
-Це третій варіант рішення :
-  for (Map.Entry<String, Integer> fruit : storage.entrySet()) {
-      fruit.setValue(fruit.getValue() + supply);
-    }
-    return storage;
-  }
- */
