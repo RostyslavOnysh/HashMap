c801a844cd9b15878e1a9c24d435847eebd545e9
Ros <rospsix@gmail.com>
2023-05-17 12:15:08 +0100

    #8 I have added a new solved task and provided an explanation of why I used Map.Entry and the foreach loop in the code

--- main/java/HomeWork/EmployeeInfo/EmployeeInfo.java
+++ main/java/HomeWork/EmployeeInfo/EmployeeInfo.java
@@ -1,12 +1,20 @@
 package HomeWork.EmployeeInfo;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class EmployeeInfo {
     public List<Employee> getEmployeesByCityAndYearOfBirth(
             Map<Employee, String> employeesInformation, int yearOfBirth, String city) {
-        //write your code here
-        return null;
+        List<Employee> employeeList = new ArrayList<>();
+        for (Map.Entry<Employee, String> employee : employeesInformation.entrySet()) {
+            if (employee.getKey().getYearOfBirth() >= yearOfBirth) {
+                if (employee.getValue().equals(city)) {
+                    employeeList.add(employee.getKey());
+                }
+            }
+        }
+        return employeeList;
     }
 }
